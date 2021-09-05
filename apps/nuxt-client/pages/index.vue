ce0b395288e4555eaf0e3449e1fcd206a9a30954
sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>
2021-09-05 16:40:16 +0200

    fix: wrong link and dead code
    Signed-off-by: sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>

--- apps/nuxt-client/pages/index.vue
+++ apps/nuxt-client/pages/index.vue
@@ -61,7 +61,7 @@
                   <p class="ml-2">
                     Get the best development experience writing components <code class="text-sm font-bold text-gray-900">without</code> touching a css using
                     <a
-                      href="https://nx.dev/"
+                      href="https://tailwindcss.com/"
                       target="_blank"
                       rel="noopener noreferrer"
                       class="text-overwrittenGreen-600"
@@ -111,39 +111,5 @@
 
 <script lang="ts">
 import Vue from "vue";
-
 export default Vue.extend({});
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-  'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-/*.subtitle {*/
-/*  font-weight: 300;*/
-/*  font-size: 42px;*/
-/*  color: #526488;*/
-/*  word-spacing: 5px;*/
-/*  padding-bottom: 15px;*/
-/*}*/
-
-.links {
-  padding-top: 15px;
-}
-</style>
