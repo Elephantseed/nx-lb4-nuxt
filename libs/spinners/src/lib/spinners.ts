319c7cf570ffebd5fa657ef15d37e0702beae9ef
sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>
2021-09-07 01:05:58 +0200

    fix: add spinner to spinner library
    Signed-off-by: sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>

--- libs/spinners/src/lib/spinners.ts
+++ libs/spinners/src/lib/spinners.ts
@@ -1,3 +1,10 @@
-export function spinners(): string {
-  return 'spinners';
+import ora, { Ora } from 'ora';
+
+export function spinners(): Ora {
+  const spinner = ora('Loading unicorns').start();
+  setTimeout(() => {
+    spinner.color = 'yellow';
+    spinner.text = 'Loading rainbows';
+  }, 1000);
+  return spinner;
 }
