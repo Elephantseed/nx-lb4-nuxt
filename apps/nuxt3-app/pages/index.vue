d97a480992746d8a33659306d31870bacaa79eee
sergiolopezbelinchon <sergio.lopez.belinchon@centralpoint.nl>
2022-03-23 11:54:22 +0100

    Release 8.19.0

--- apps/nuxt3-app/pages/index.vue
+++ apps/nuxt3-app/pages/index.vue
@@ -98,7 +98,6 @@
             </div>
             <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
               <Counter />
-              sergio4545
             </div>
           </div>
         </div>
